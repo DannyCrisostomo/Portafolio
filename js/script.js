27c1ce71d937b0681ed0b7b97ae9584b1cc0a005
Danny  Crisostomo <146300252+DannyCrisostomo@users.noreply.github.com>
2023-10-30 19:26:19 -0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ window.addEventListener("load", function () {
   setTimeout(function () {
     document.querySelector(".contenedor").style.display = "none";
     showNewScreen();
-  }, 5000); // Esperar 5 segundos antes de mostrar la nueva pantalla
+  }, 1000); // Esperar 5 segundos antes de mostrar la nueva pantalla
 });
 
 
@@ -12,7 +12,7 @@ function showNewScreen() {
   // Aquí puedes realizar la acción para mostrar la nueva pantalla
   const newScreen = document.createElement("div");
   newScreen.innerHTML = `
-  <div class="windows">
+  <div id="windows" >
     <div class="navegar">
         <div class="superior">
             <div class="icono">
@@ -86,6 +86,12 @@ function showNewScreen() {
                     </a>
                 </div>
 
+                <div class="apk">
+                    <a class="link" href="#">
+                        <img class="icono-esc" src="./icon/mapa.png" alt=""> Maps
+                    </a>
+                </div>
+
 
         </div>
     </div>
@@ -109,11 +115,26 @@ function showNewScreen() {
       document.getElementById("dia").textContent = diaSemana;
       document.getElementById("fecha").textContent = `${dia}/${mes}/${anio}`;
     }
-  
     // Llamar a la función para mostrar la hora y la fecha inicialmente
     actualizarHoraFecha();
-  
     // Actualizar la hora y la fecha cada segundo
     setInterval(actualizarHoraFecha, 1000);
+   // Cambiar el fondo
+  const windowsContainer = document.getElementById("windows");
+  const backgrounds = ["./img/fondo.jpg", "./img/fondo1.jpg", "./img/fondo2.jpg", "./img/fondo3.jpg",];
+  let currentBackgroundIndex = 0;
+
+  function changeBackground() {
+    windowsContainer.style.backgroundImage = `url("${backgrounds[currentBackgroundIndex]}")`;
+
+    currentBackgroundIndex++;
+    if (currentBackgroundIndex >= backgrounds.length) {
+      currentBackgroundIndex = 0;
+    }
   }
-  
+
+  changeBackground(); // Cambia el fondo inicial
+  setInterval(changeBackground, 10000); //Cambia el fondo cada 10 segundo
+  //setInterval(changeBackground, 30 * 60 * 1000);  Cambia el fondo cada 30 minutos
+
+  }
