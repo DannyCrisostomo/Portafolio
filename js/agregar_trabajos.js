fbdac1ba6a47a91e53e5f80c71e0c2a263e9c8a3
Danny  Crisostomo <146300252+DannyCrisostomo@users.noreply.github.com>
2024-01-20 09:51:09 -0500

    Add files via upload

---
+++ js/agregar_trabajos.js
@@ -0,0 +1,89 @@
+const datos_musica = [
+    {
+        link: "https://www.linkedin.com/in/danny-crisostomo/",
+        imagen: "./icon/trabajos/linkedin.png",
+        nombre:"Linkedin",
+    },
+    {
+        link: "https://walink.co/4f87d6",
+        imagen: "./icon/trabajos/whatsapp.png",
+        nombre:"WhatsApp",
+    },
+    {
+        link: "https://www.linkedin.com/in/danny-crisostomo/",
+        imagen: "./icon/trabajos/mapa.png",
+        nombre:"Maps",
+    },
+    {
+        link: "https://dazzling-axolotl-8b7918.netlify.app/",
+        imagen: "./icon/trabajos/qr.png",
+        nombre:"QR",
+    },
+    {
+        link: "https://frolicking-pasca-b144ec.netlify.app/",
+        imagen: "./icon/trabajos/Scroll.png",
+        nombre:"Scroll",
+    },
+    {
+        link: "https://frabjous-praline-7e480f.netlify.app/",
+        imagen: "./icon/trabajos/StarBuck.png",
+        nombre:"StarBuck",
+    },
+    {
+        link: "https://dreamy-zuccutto-e94890.netlify.app/",
+        imagen: "./icon/trabajos/Netflix.png",
+        nombre:"Netflix",
+    },
+    {
+        link: "https://polite-praline-75cd4b.netlify.app/",
+        imagen: "./icon/trabajos/Portada.png",
+        nombre:"Portada",
+    },
+    {
+        link: "https://tiny-lollipop-6064e4.netlify.app/",
+        imagen: "./icon/trabajos/Carta.png",
+        nombre:"Carta",
+    },
+    {
+        link: "https://radiant-otter-54ab8e.netlify.app/",
+        imagen: "./icon/trabajos/Potencia_tu_venta.png",
+        nombre:"Venta",
+    },
+    {
+        link: "https://gorgeous-zuccutto-26cf6c.netlify.app/",
+        imagen: "./icon/trabajos/Hora.png",
+        nombre:"Hora",
+    },
+    {
+        link: "https://hilarious-gingersnap-2e7320.netlify.app/",
+        imagen: "./icon/trabajos/BlogCoffe.png",
+        nombre:"BlogCoffe",
+    },
+    {
+        link: "https://sparkling-selkie-fe7376.netlify.app/",
+        imagen: "./icon/trabajos/SpeedBur.png",
+        nombre:"SpeedBur",
+    },
+    {
+        link: "https://luminous-narwhal-6b7616.netlify.app/",
+        imagen: "./icon/trabajos/Goku.png",
+        nombre:"DBZ",
+    },
+    {
+        link: "https://thunderous-klepon-c3991e.netlify.app/",
+        imagen: "./icon/trabajos/Blog.png",
+        nombre:"Blog",
+    },
+];
+
+const insertar_datos = document.getElementById("agregar_trabajos");
+for (let index of datos_musica) {
+    insertar_datos.innerHTML += `
+    
+    <div class="apk">
+        <a class="link" href="${index.link}"">
+            <img class="icono-esc" src="${index.imagen}" alt="">${index.nombre}
+        </a>
+    </div>
+`;
+}
--- js/script.js
+++ js/script.js
@@ -1,140 +1,50 @@
 // Simular la carga
 window.addEventListener("load", function () {
-  setTimeout(function () {
-    document.querySelector(".contenedor").style.display = "none";
-    showNewScreen();
-  }, 5000); // Esperar 5 segundos antes de mostrar la nueva pantalla
+    setTimeout(function () {
+        document.querySelector(".contenedor").style.display = "none";
+        showNewScreen();
+    }, 1500); // Esperar 5 segundos antes de mostrar la nueva pantalla
+});
+// Cambia de carga a escritorio
+window.addEventListener("load", function () {
+    setTimeout(function () {
+        document.querySelector(".contenedor_windows").style.display = "block";
+        showNewScreen();
+    }, 1500); // Esperar 5 segundos antes de mostrar la nueva pantalla
 });
 
+// Función para obtener y mostrar la hora y la fecha en tiempo real
+function actualizarHoraFecha() {
+    const horaFecha = new Date();
+    const hora = horaFecha.getHours().toString().padStart(2, "0");
+    const minutos = horaFecha.getMinutes().toString().padStart(2, "0");
+    const dia = horaFecha.getDate().toString().padStart(2, "0");
+    const mes = (horaFecha.getMonth() + 1).toString().padStart(2, "0");
+    const anio = horaFecha.getFullYear();
+    const diaSemana = ["Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"][horaFecha.getDay()];
 
-// Nueva pantalla
-function showNewScreen() {
-  // Aquí puedes realizar la acción para mostrar la nueva pantalla
-  const newScreen = document.createElement("div");
-  newScreen.innerHTML = `
-  <div id="windows" >
-    <div class="navegar">
-        <div class="superior">
-            <div class="icono">
-                <a class="link" href="youtuve.com">
-                    <img class="link-icono" src="./icon/ventanas.png" alt="">
-                </a>
-            </div>
-            <div class="icono">
-                <a class="link" href="youtuve.com">
-                    <img class="link-icono" src="./icon/lupa.png" alt="">
-                </a>
-            </div>
-            <div class="icono">
-                <a class="link" href="https://github.com/DannyCrisostomo">
-                    <img class="link-icono" src="./icon/github.png" alt="">
-                </a>
-            </div>
-            <div class="icono">
-                <a class="link" href="https://www.youtube.com/">
-                    <img class="link-icono" src="./icon/youtuve.png" alt="">
-                </a>
-            </div>
-            <div class="icono">
-                <a class="link"
-                    href="https://www.google.com/webhp?hl=es-419&sa=X&ved=0ahUKEwivzpOp242CAxWpqpUCHYNuAssQPAgJ">
-                    <img class="link-icono" src="./icon/chrome.png" alt="">
-                </a>
-            </div>
-        </div>
-        <div class="inferior">
+    document.getElementById("hora").textContent = `${hora}:${minutos}`;
+    document.getElementById("dia").textContent = diaSemana;
+    document.getElementById("fecha").textContent = `${dia}/${mes}/${anio}`;
+}
+// Llamar a la función para mostrar la hora y la fecha inicialmente
+actualizarHoraFecha();
+// Actualizar la hora y la fecha cada segundo
+setInterval(actualizarHoraFecha, 1000);
+// Cambiar el fondo
 
-            <div class="icono">
-                <div class="funciones sonido">
-                    <img class="link-icono" src="./icon/grabar.png" alt="">
-                    <img class="link-icono" src="./icon/internet.png" alt="">
-                    <img class="link-icono" src="./icon/sonido.png" alt="">
-                </div> 
-            </div>
-            <div class="icono">
-                <a class="link lenguaje" href="#">
-                    <h5>ESP</h5>
-                </a>
-            </div>
+const windowsContainer = document.getElementById("windows");
+const backgrounds = ["./img/fondo.jpg", "./img/fondo1.jpg", "./img/fondo2.jpg", "./img/fondo3.jpg",];
+let currentBackgroundIndex = 0;
 
-            <div class="icono fecha">
-                <div id="hora"></div>
-                <div id="dia"></div>
-                <div id="fecha"></div>
-            </div>
-
-             <div class="icono">
-                <a class="link" href="#">
-                    <img class="link-icono" src="./icon/notificacion.png" alt="">
-                </a>
-            </div>
-        </div>
-    </div>
-    <div class="escritorio">
-        <div class="contenedor-app app-columnas">
-
-
-                <div class="apk">
-                    <a class="link" href="https://www.linkedin.com/in/danny-crisostomo/">
-                        <img class="icono-esc" src="./icon/linkedin.png" alt=""> Linkedin
-                    </a>
-                </div>
-    
-                <div class="apk">
-                    <a class="link" href="https://walink.co/4f87d6">
-                        <img class="icono-esc" src="./icon/whatsapp.png" alt=""> whatsapp
-                    </a>
-                </div>
-
-                <div class="apk">
-                    <a class="link" href="#">
-                        <img class="icono-esc" src="./icon/mapa.png" alt=""> Maps
-                    </a>
-                </div>
-
-
-        </div>
-    </div>
-</div>
-    `;
-  document.body.appendChild(newScreen);
-
-
-  
-    // Función para obtener y mostrar la hora y la fecha en tiempo real
-    function actualizarHoraFecha() {
-      const horaFecha = new Date();
-      const hora = horaFecha.getHours().toString().padStart(2, "0");
-      const minutos = horaFecha.getMinutes().toString().padStart(2, "0");
-      const dia = horaFecha.getDate().toString().padStart(2, "0");
-      const mes = (horaFecha.getMonth() + 1).toString().padStart(2, "0");
-      const anio = horaFecha.getFullYear();
-      const diaSemana = ["Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"][horaFecha.getDay()];
-  
-      document.getElementById("hora").textContent = `${hora}:${minutos}`;
-      document.getElementById("dia").textContent = diaSemana;
-      document.getElementById("fecha").textContent = `${dia}/${mes}/${anio}`;
-    }
-    // Llamar a la función para mostrar la hora y la fecha inicialmente
-    actualizarHoraFecha();
-    // Actualizar la hora y la fecha cada segundo
-    setInterval(actualizarHoraFecha, 1000);
-   // Cambiar el fondo
-  const windowsContainer = document.getElementById("windows");
-  const backgrounds = ["./img/fondo.jpg", "./img/fondo1.jpg", "./img/fondo2.jpg", "./img/fondo3.jpg",];
-  let currentBackgroundIndex = 0;
-
-  function changeBackground() {
+function changeBackground() {
     windowsContainer.style.backgroundImage = `url("${backgrounds[currentBackgroundIndex]}")`;
-
     currentBackgroundIndex++;
     if (currentBackgroundIndex >= backgrounds.length) {
-      currentBackgroundIndex = 0;
+        currentBackgroundIndex = 0;
     }
-  }
+}
 
-  changeBackground(); // Cambia el fondo inicial
-  setInterval(changeBackground, 10000); //Cambia el fondo cada 10 segundo
-  //setInterval(changeBackground, 30 * 60 * 1000);  Cambia el fondo cada 30 minutos
+changeBackground(); // Cambia el fondo inicial
+setInterval(changeBackground, 300 * 60 * 1000);  //Cambia el fondo cada 300 minutos
 
-  }
